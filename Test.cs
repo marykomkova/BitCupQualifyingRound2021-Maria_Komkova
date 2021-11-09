f2baed4ea234275be91f95e3bce211b00f90a8b7
Mary <marykomkova04@gmail.com>
2021-11-09 21:19:27 +0300

    Open cloud.google.com

---
+++ Test.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Testing
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://cloud.google.com/";
+        }
+    }
+}
