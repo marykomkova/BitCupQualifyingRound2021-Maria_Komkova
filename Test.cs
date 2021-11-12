31504140c27a9521e8e0d7b2f4fecf0020844eb2
Mary <marykomkova04@gmail.com>
2021-11-12 19:43:36 +0300

    Open calculator page

--- Test.cs
+++ Test.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace Testing
 {
@@ -10,6 +11,16 @@ namespace Testing
         {
             IWebDriver driver = new ChromeDriver();
             driver.Url = "https://cloud.google.com/";
+            IWebElement search = driver.FindElement(By.Name("q"));
+            search.Click();
+            search.Clear();
+            search.SendKeys("Google Cloud Platform Pricing Calculator");
+            search.SendKeys(Keys.Enter);
+            Thread.Sleep(5000);
+            driver.FindElement(By.LinkText("Google Cloud Platform Pricing Calculator")).Click();
+            
+            Thread.Sleep(10000);
+            driver.Quit();
         }
     }
 }
